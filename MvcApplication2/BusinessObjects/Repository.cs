327602182185edd88cc2cd6ee935c4fa97c8fcd1
burcevsemyon <burcevsemyon@gmail.com>
2015-06-29 20:06:07 +0300

    репозиторий

---
+++ MvcApplication2/BusinessObjects/Repository.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using GasOil.DataModel;
+
+namespace MvcApplication2.BusinessObjects
+{
+    public class Repository
+    {
+        private readonly GasOilEntities _context;
+
+        public Repository(GasOilEntities context)
+        {
+            _context = context;
+        }
+
+        public Product GetProduct(long productId)
+        {
+            return _context.Products.SingleOrDefault(p => p.Id == productId);
+        }
+
+        public List<Product> GetProductsByGroup(long? groupId)
+        {
+            return _context.Products.Where(p => p.GroupId == groupId).ToList();
+        }
+
+        public List<UnitOfMeasurement> GetUnitOfMeasurement()
+        {
+            return _context.UnitOfMeasurements.ToList();
+        }
+    }
+}
